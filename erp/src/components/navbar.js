f6d0958f747768f2f44ee28210d5eac82e3a68d2
muskbuster <sudeepkamat79@gmail.com>
2022-09-06 10:26:31 +0000

    img

--- erp/src/components/navbar.js
+++ erp/src/components/navbar.js
@@ -1,10 +1,11 @@
 import "/workspace/erpformelements/erp/src/components/navbar.css";
 import { Outlet, Link } from "react-router-dom";
+import omni from "/workspace/erpformelements/erp/src/components/omni.png";
 export default function Navbar(){
 return(
     <>
     <header className='navbar'>
-        <div className='navbar__title navbar__item'>Omniforge</div>
+        <div className='navbar__title navbar__item'><img className="adjust" src={omni}/></div>
         <Link to='/orders' className='navbar__item'>Orders</Link>
         <Link to="/product" className='navbar__item'>Products</Link>     
     </header>
